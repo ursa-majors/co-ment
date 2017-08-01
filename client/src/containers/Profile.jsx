5864a35b5c796f19a92f894aa9c0e8bc7c36b8af
genestd <drwgenest@gmail.com>
2017-08-01 16:18:46 -0700

    add initial profile component

---
+++ client/src/containers/Profile.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import axios from 'axios';
+import * as Actions from '../store/actions';
+
+class Profile extends React.Component {
+
+  componentWillMount() {
+    axios.get(`/api/profile/${this.props.appState.userID}`, {
+      headers: {
+        Authentication: `Bearer ${this.props.appState.authToken}`,
+      },
+    })
+    .then((response) => {
+      console.log('response', response);
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  appState: state.appState,
+  profile: state.profile,
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(Actions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
