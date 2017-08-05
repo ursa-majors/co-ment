369a282427096b52506f183ffda4dc1194a32b06
genestd <drwgenest@gmail.com>
2017-08-05 08:58:36 -0700

    Add viewpost route to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import Posts from './containers/Posts';
 import AddPost from './containers/AddPost';
 import Logout from './containers/Logout';
 import MentorPath from './containers/MentorPath';
+import ViewPost from './containers/ViewPost';
 
 class App extends React.Component {
 
@@ -31,6 +32,7 @@ class App extends React.Component {
               <Route path="/profile" component={Profile} />
               <Route path="/posts" component={Posts} />
               <Route path="/logout" component={Logout} />
+              <Route path="/viewpost/:id" component={ViewPost} />
               <Route path="/addpost" component={AddPost} />
               <Route path="/mentorpath" component={MentorPath} />
             </Switch>
