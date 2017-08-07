f3719f0d6313368dcc89e1e03ed9a8bc890a907b
Jay Schwane <jrschwane@uwalumni.com>
2017-08-06 19:29:05 -0500

    Modify API contact route to fetch sender's email address from db

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -435,38 +435,54 @@ routes.post('/api/contact/:id', auth, (req, res) => {
     }
     
     const target = req.params.id;
+    const sender = req.token._id;
     
-    
-    User.findOne({_id: target}, (err, profile) => {
+    // find the target recipient
+    User.findOne({_id: target})
+        .exec()
+        .then(recipient => {
         
-        if (!profile) {
-            return res
-                .status(404)
-                .json({ message : 'User not found!'});
-        }
+            if (!recipient) {
+                return res
+                    .status(404)
+                    .json({ message : 'User not found!'});
+            } else {
+                return recipient;
+            }
+        })
+        .then(recipient => {
         
-        // what do we want to include in the message? Hmm ...
-        const bodyText = req.body.bodyText;
+            // find the sender (we need their email address)
+            User.findOne({_id: sender}, (err, sender) => {
+                
+                if (err) { throw err; }
+                
+                // what do we want to include in the message? Hmm ...
+                const bodyText = req.body.bodyText;
+
+                const from_user  = sender.username;
+                const from_email = sender.email;
+                const to         = recipient.email;
+                const subject    = `co/ment - Contact Request from ${from_user}`;
+                const body       = `Contact Request from ${from_user} (${from_email}).\n\n${bodyText}`;
+
+                // send mail using `mailer` util
+                try {
+                    mailer(to, subject, body);
+                    return res
+                        .status(200)
+                        .json({ message : 'Message sent successfully.'});
+                } catch (err) {
+                    console.log(`Error: $(err)`);
+                    return res
+                        .status(400)
+                        .json({ message : 'Error: Message not sent.'});
+                }
+                
+            });
         
-        const from    = req.token.username;
-        const to      = profile.email;
-        const subject = `co/ment - Contact Request from ${from}`;
-        const body    = `Contact Request from ${from}:\n\n${bodyText}`;
+        });
         
-        // send mail using `mailer` util
-        try {
-            mailer(to, subject, body);
-            return res
-                .status(200)
-                .json({ message : 'Message sent successfully.'});
-        } catch (err) {
-            console.log(`Error: $(err)`);
-            return res
-                .status(400)
-                .json({ message : 'Error: Message not sent.'});
-        }
-        
-    });
     
 });
 
