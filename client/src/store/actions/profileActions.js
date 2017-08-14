ebd0017341e1cd104104b937e8cf98d1b918bfdf
genestd <drwgenest@gmail.com>
2017-08-14 15:36:21 -0700

    Fixing the ViewProfile component to show the updated profile.

--- client/src/store/actions/profileActions.js
+++ client/src/store/actions/profileActions.js
@@ -6,7 +6,7 @@ export const ADD_SKILL = 'ADD_SKILL';
 export const ADD_LANGUAGE = 'ADD_LANGUAGE';
 export const REMOVE_SKILL = 'REMOVE_SKILL';
 export const REMOVE_LANGUAGE = 'REMOVE_LANGUAGE';
-
+export const DISMISS_VIEWPROFILE_MODAL = 'DISMISS_VIEWPROFILE_MODAL';
 
 export function saveProfile(profile) {
   return ({
@@ -62,5 +62,12 @@ export function removeSkill(word) {
   return ({
     type: REMOVE_SKILL,
     payload: word,
+  });
+}
+
+export function dismissViewProfileModal() {
+  console.log('dvpm')
+  return ({
+    type: DISMISS_VIEWPROFILE_MODAL,
   });
 }
