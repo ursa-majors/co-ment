388dbbca9d724bff58b12bcdb851a8602fb9d969
genestd <drwgenest@gmail.com>
2017-08-15 15:57:11 -0700

    Front end changes to support connection lifecycle.

--- client/src/containers/ConnectionDetails.jsx
+++ client/src/containers/ConnectionDetails.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
-import * as apiActions from '../store/actions/apiActions';
+import * as apiActions from '../store/actions/apiConnectionActions';
 import * as Actions from '../store/actions/connectionActions';
 
 
@@ -21,30 +21,64 @@ class ConnectionDetails extends React.Component {
     this.props.actions.clearViewConnection();
   }
 
-  render() {
+  getActions = () => {
     let actions;
     switch (this.props.connection.viewConnection.status) {
       case 'pending':
-        actions = (
-          <ul className="post-nav">
+        actions = (this.props.connection.viewConnection.initiator.id !== this.props.profiles.userProfile._id) ?
+          (<ul className="post-nav">
             <li className="post-nav__item" >
-              <span className="post-nav__item-link pointer" onClick={() => null}>
+              <span
+                className="post-nav__item-link pointer"
+                onClick={() =>
+                  this.props.api.updateConnectionStatus(
+                    this.props.appState.authToken,
+                    {
+                      id: this.props.connection.viewConnection._id,
+                      type: 'ACCEPT',
+                    },
+                  )
+                }
+              >
                 Accept
               </span>
             </li>
             <li className="post-nav__item" >
-              <span className="post-nav__item-link pointer" onClick={() => null}>
+              <span
+                className="post-nav__item-link pointer"
+                onClick={() =>
+                  this.props.api.updateConnectionStatus(
+                    this.props.appState.authToken,
+                    {
+                      id: this.props.connection.viewConnection._id,
+                      type: 'DECLINE',
+                    },
+                  )
+                }
+              >
                 Decline
               </span>
             </li>
           </ul>
-        );
+        ) :
+        null;
         break;
-      case 'active':
+      case 'accepted':
         actions = (
           <ul className="post-nav">
             <li className="post-nav__item" >
-              <span className="post-nav__item-link pointer" onClick={() => null}>
+              <span
+                className="post-nav__item-link pointer"
+                onClick={() =>
+                  this.props.api.updateConnectionStatus(
+                    this.props.appState.authToken,
+                    {
+                      id: this.props.connection.viewConnection._id,
+                      type: 'EXPIRE',
+                    },
+                  )
+                }
+              >
                 Finish
               </span>
             </li>
@@ -56,7 +90,11 @@ class ConnectionDetails extends React.Component {
       default:
         actions = null;
     }
+    return actions;
+  }
 
+  render() {
+    console.log(this.props.connection)
     const conn = this.props.connection.viewConnection;
     return (
       <div className="container conn-details">
@@ -66,14 +104,14 @@ class ConnectionDetails extends React.Component {
           </div>
         </div>
         <div className="conn-details__avatars">
-            <div className="conn-details__image-wrap">
-              <div>Mentor</div>
-              <img className="conn-details__image" src={this.props.connection.viewConnection.mentor.avatar || null} />
-            </div>
-            <div className="conn-details__image-wrap">
-              <div>Mentee</div>
-              <img className="conn-details__image" src={this.props.connection.viewConnection.mentee.avatar || null} />
-            </div>
+          <div className="conn-details__image-wrap">
+            <div>Mentor</div>
+            <img className="conn-details__image" src={this.props.connection.viewConnection.mentor.avatar || null} />
+          </div>
+          <div className="conn-details__image-wrap">
+            <div>Mentee</div>
+            <img className="conn-details__image" src={this.props.connection.viewConnection.mentee.avatar || null} />
+          </div>
         </div>
         <div className="conn-details__text-wrap">
           Initiated By: {this.props.connection.viewConnection.initiator.name}
@@ -91,16 +129,18 @@ class ConnectionDetails extends React.Component {
           Date Updated: {this.props.connection.viewConnection.dateStarted}
         </div>
         <div className="single-post__button-wrap">
-            { actions }
+          { this.getActions() }
         </div>
       </div>
     );
   }
 }
 
+
 const mapStateToProps = state => ({
+  appState: state.appState,
   connection: state.connection,
-  profiles: state.profiles
+  profiles: state.profiles,
 });
 
 const mapDispatchToProps = dispatch => ({
--- client/src/store/reducers/connection.js
+++ client/src/store/reducers/connection.js
@@ -1,7 +1,11 @@
+import update from 'immutability-helper';
+
 import { SET_VIEW_CONNECTION, CLEAR_VIEW_CONNECTION } from '../actions/connectionActions';
 import { CONTACT_REQUEST, CONTACT_SUCCESS, CONTACT_FAILURE } from '../actions/apiActions';
 import { CONNECTION_REQUEST, CONNECTION_SUCCESS, CONNECTION_FAILURE,
   GET_ALL_CONNECTIONS_REQUEST, GET_ALL_CONNECTIONS_SUCCESS, GET_ALL_CONNECTIONS_FAILURE,
+  UPDATE_CONNECTION_STATUS_REQUEST, UPDATE_CONNECTION_STATUS_SUCCESS,
+  UPDATE_CONNECTION_STATUS_FAILURE,
 } from '../actions/apiConnectionActions';
 
 const defaultConn = {
@@ -40,10 +44,15 @@ const INITIAL_STATE = {
   getConnectionsError: null,
   connections: [],
   viewConnection: defaultConn,
+  updateConnectionSpinnerClass: 'spinner__hide',
+  updateConnectionModalClass: 'modal__hide',
+  updateConnectionModalText: '',
+  updateConnectionError: '',
 };
 
 function connection(state = INITIAL_STATE, action) {
   let error;
+  let index;
   switch (action.type) {
 
     case SET_VIEW_CONNECTION:
@@ -76,11 +85,58 @@ function connection(state = INITIAL_STATE, action) {
       return Object.assign({}, state, { getConnectionsLoading: true, getConnectionsError: null });
 
     case GET_ALL_CONNECTIONS_SUCCESS:
-      return Object.assign({}, state, { connections: action.payload.connections, getConnectionsLoading: false, getConnectionsError: null });
+      return Object.assign(
+        {},
+        state,
+        {
+          connections: action.payload.connections,
+          getConnectionsLoading: false,
+          getConnectionsError: null,
+        },
+      );
 
     case GET_ALL_CONNECTIONS_FAILURE:
       error = action.payload.data || { message: action.payload.message };
       return Object.assign({}, state, { getConnectionsLoading: false, getConnectionsError: error });
+
+    case UPDATE_CONNECTION_STATUS_REQUEST:
+      return Object.assign(
+        {},
+        state,
+        {
+          updateConnectionSpinnerClass: 'spinner__show',
+          updateConnectionError: '',
+        },
+      );
+
+    case UPDATE_CONNECTION_STATUS_SUCCESS:
+      index = -1;
+      for (index = 0; index < state.connections.length; index += 1) {
+        if (state.connections[index]._id === action.payload._id) {
+          break;
+        }
+      }
+      return update(
+        state,
+        {
+          updateConnectionSpinnerClass: { $set: 'spinner__hide' },
+          updateConnectionError: { $set: '' },
+          updateConnectionModalClass: { $set: 'modal__show' },
+          updateConnectionModalText: { $set: 'success' },
+          connections: { $splice: [[index, 1, action.payload.conn]] },
+          viewConnection: { $set: action.payload.conn },
+        },
+      );
+
+    case UPDATE_CONNECTION_STATUS_FAILURE:
+      return Object.assign(
+        {},
+        state,
+        {
+          updateConnectionSpinnerClass: 'spinner__hide',
+          updateConnectionError: 'An error occurred',
+        },
+      );
 
     default:
       return state;
