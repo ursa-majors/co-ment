006d03a97ef0ad6d2fd713ece80efcf154d923cb
genestd <drwgenest@gmail.com>
2017-08-17 16:21:03 -0700

    Fix error from moving posts actions to new file.

--- client/src/containers/MentorPath.jsx
+++ client/src/containers/MentorPath.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import * as apiActions from '../store/actions/apiActions';
+import * as apiActions from '../store/actions/apiPostActions';
 import * as Actions from '../store/actions/postActions';
 
 const searchObj = {
