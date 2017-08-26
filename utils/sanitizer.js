af69388a03d5bd510df0e5a845b509a4f019a0b4
Jay Schwane <jrschwane@uwalumni.com>
2017-08-25 22:22:27 -0500

    Adds module to strip nefarious markup text (scripts, etc) from strings

---
+++ utils/sanitizer.js
@@ -0,0 +1,37 @@
+/* utils/sanitizer.js
+
+   Strips nefarious BS from a string
+   
+   Credit:
+   https://stackoverflow.com/questions/295566/sanitize-rewrite-html-on-the-client-side
+   
+*/
+
+const tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+const tagOrComment = new RegExp(
+    '<(?:' +
+    // Comment body.
+    '!--(?:(?:-*[^->])*--+|-?)' +
+    // Special "raw text" elements whose content should be elided.
+    '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*' +
+    '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*' +
+    // Regular name
+    '|/?[a-z]' + tagBody + ')>',
+    'gi');
+
+
+function removeTags(html) {
+    let oldHtml;
+    do {
+        oldHtml = html;
+        html = html.replace(tagOrComment, '');
+    } while (html !== oldHtml);
+    return html.replace(/</g, '&lt;');
+}
+
+
+module.exports = (text) => {
+    
+    return removeTags(text);
+    
+};
