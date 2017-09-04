7b66d9ae1113000bffb8e0116aa7e8a1a40182f7
genestd <drwgenest@gmail.com>
2017-09-04 15:21:47 -0700

    First draft of about screen.

--- client/src/containers/About.jsx
+++ client/src/containers/About.jsx
@@ -1,41 +1,89 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
+import PropTypes from 'prop-types';
 import * as Actions from '../store/actions/tourActions';
 
-const About = props => (
-  <div className="container">
-    <h2 className="tour-title">About</h2>
+
+const About = (props) => {
+  const style = {
+    backgroundImage: `url(${props.tour.slides[props.tour.imageIndex].image}`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+  };
+  return (
     <div className="tour">
       <div className="tour__slide">
         <div className="tour__slide-header">
           {props.tour.slides[props.tour.imageIndex].title}
         </div>
-        <div>
-          <img className="tour__image" src={props.tour.slides[props.tour.imageIndex].image} />
+        <div className="tour__slide-content">
+          <div className="tour__slide-text" dangerouslySetInnerHTML={props.tour.slides[props.tour.imageIndex]}>
+          </div>
+          <div className="tour__image">
+            <img src={`${props.tour.slides[props.tour.imageIndex].image}`} alt={props.tour.slides[props.tour.imageIndex].imageAlt} />
+          </div>
+        </div>
+        <div className="tour__chevron-left">
+          <i
+            className="fa fa-chevron-left"
+            onClick={
+              () => {
+                let newIndex = props.tour.imageIndex - 1;
+                if (newIndex < 0) {
+                  newIndex = props.tour.slides.length - 1;
+                }
+                props.actions.setIndex(newIndex);
+              }
+            }
+          />
+        </div>
+        <div className="tour__chevron-right">
+          <i
+            className="fa fa-chevron-right"
+            onClick={
+              () => {
+                let newIndex = props.tour.imageIndex + 1;
+                if (newIndex > props.tour.slides.length - 1) {
+                  newIndex = 0;
+                }
+                props.actions.setIndex(newIndex);
+              }
+            }
+          />
+        </div>
+        {/* Dot-controls for carousel */}
+        <div className="tour__controls">
+          {
+            props.tour.slides.map((slide, index) => {
+              const active = (props.tour.imageIndex === index ? '-active' : '');
+              return (
+                <div
+                  className={`tour__dot${active}`}
+                  key={slide.slideId}
+                  id={index}
+                  onClick={(e) => { props.actions.setIndex(parseInt(e.target.id)); }}
+                >
+                  <i className="fa fa-circle" id={index} />
+                </div>
+              );
+            })
+          }
         </div>
       </div>
-      {/* Dot-controls for carousel */}
-      <div className="tour__controls">
-        {
-          props.tour.slides.map((slide,index) => {
-            return (
-              <div
-                className="tour__dot"
-                key={slide+index}
-                id={index}
-                onClick={(e) => { props.actions.setIndex(e.target.id); }}
-              >
-                <i className="fa fa-circle" id={index}/>
-              </div>
-            );
-          })
-        }
-      </div>
     </div>
-  </div>
-);
+  );
+}
+
+About.propTypes = {
+  tour: PropTypes.shape({
+    imageIndex: PropTypes.Number,
+    slides: PropTypes.Array,
+  }).isRequired,
+  actions: PropTypes.shape({
+    setIndex: PropTypes.func,
+  }).isRequired,
+};
 
 const mapStateToProps = state => ({
   tour: state.tour,
--- client/src/utils/slides.js
+++ client/src/utils/slides.js
@@ -2,22 +2,46 @@ const slides = [
   {
     title: 'Introduction',
     image: '',
-    html: '',
+    imageAlt: '',
+    __html: `<div>
+    <p>Welcome to Co/Ment - a community to connect Mentors and Mentees to accelerate their code journey</p>
+    <p>The site features the tools to help you find a coding partner tailored to your needs - from matching your technical background, to your spoken language...even your timezone.</p>
+    <p>No matter where you stand on the coding path, there are others both ahead of you and behind.  There's no requirement to becoming a mentor - just a willingness to share your knowledge.</p>
+    </div>`,
+    slideId: 1,
   },
   {
     title: 'Step 1: Profile',
     image: './src/img/profile.png',
-    html: '',
+    imageAlt: 'Profile',
+    __html: `<div>
+    <p>Begin by filling out your profile</p>
+    <p>Add skills, languages, timezone and links to your social profiles</p>
+    <p>This information will help you find the perfect contact</p>
+    </div>`,
+    slideId: 2,
   },
   {
     title: 'Step 2: Posts',
     image: './src/img/post.png',
-    html: '',
+    imageAlt: 'Posts',
+    __html: `<div>
+    <p>Next, create a post advertising your desired match.</p>
+    <p>Other users can connect to you through your post.</p>
+    <p>You will be notified by an email when a user wants to connect.</p>
+    </div>`,
+    slideId: 3,
   },
   {
     title: 'Step 3: Connections',
     image: './src/img/connection.gif',
-    html: '',
+    imageAlt: 'Connections',
+    __html: `<div>
+    <p>You can request a connection when you a find a post that matches your needs.</p>
+    <p>The user will receive an email with your request</p>
+    <p>Once you are connected, the mentorship can proceed through eMail, Slack or even in-person meetings.</p>
+    </div>`,
+    slideId: 4,
   },
 ];
 
