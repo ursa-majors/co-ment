5459da77c6040bc8a6cf609fde28fdc8b6c6b6bc
Sarah Schneider <rifkegribenes@gmail.com>
2017-09-12 15:44:51 -0700

    update mailtemplates & contact.ctr.js

--- controllers/contact.ctrl.js
+++ controllers/contact.ctrl.js
@@ -22,7 +22,6 @@ const emailTpl  = require('../utils/mailtemplates');
 //   Returns: success message on success
 //
 function sendEmail(req, res) {
-
     // prohibit users from contacing themselves
     if (req.token._id === req.params.id) {
         return res
@@ -52,7 +51,7 @@ function sendEmail(req, res) {
             User.findOne(fromId, (err, sender) => {
 
                 if (err) { throw err; }
-                             
+
                 const params = {
                     to      : recipient.email,
                     subject : `co/ment - Contact Request from ${sender.username}`,
@@ -67,7 +66,7 @@ function sendEmail(req, res) {
                                )
                     }
                 };
-                
+
                 // send mail using `mailer` util
                 try {
                     mailer(params.to, params.subject, params.body);
@@ -75,7 +74,103 @@ function sendEmail(req, res) {
                         .status(200)
                         .json({ message : 'Message sent successfully.'});
                 } catch (err) {
-                    console.log(`Error: $(err)`);
+                    console.log(`Error: ${err}`);
+                    return res
+                        .status(400)
+                        .json({ message : 'Error: Message not sent.'});
+                }
+
+            });
+
+        });
+}
+
+// SEND EMAIL2
+//   Example: POST >> /api/sendemail
+//   Secured: yes, valid JWT required
+//   Expects:
+//     1) Object with email options
+//        { recipient: String (username),
+//          sender: String (username),
+//          copySender: boolean,
+//          subject: String,
+//          body: String,
+//          type: String,
+//    };
+//   Returns: success message on success
+function sendEmail2(req, res) {
+
+    // prohibit users from contacing themselves
+    if (req.token.username === req.body.recipient) {
+        return res
+            .status(400)
+            .json({ message : 'You cannot contact yourself!'});
+    }
+
+    const target = { username: req.body.recipient };
+    const fromId = { username: req.body.sender };
+
+    // find the target recipient
+    User.findOne(target)
+        .exec()
+        .then(recipient => {
+
+            if (!recipient) {
+                return res
+                    .status(404)
+                    .json({ message : 'User not found!'});
+            } else {
+                return recipient;
+            }
+        })
+        .then(recipient => {
+            // find the sender (we need their email address)
+            User.findOne(fromId, (err, sender) => {
+
+                if (err) { throw err; }
+
+                const recipientList = req.body.copySender ? `${recipient.email};${sender.email}` : recipient.email;
+                let boilerplate = '';
+                switch(req.body.type) {
+                  case 'request':
+                    boilerplate = `Great news! <strong>${sender.username}</strong> (<a class="link" href="mailto:${sender.email}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${sender.email}</a>) has responded to your ad on co/ment!`;
+                    break;
+                  case 'accept':
+                    boilerplate = `Great news! <strong>${sender.username}</strong> (<a class="link" href="mailto:${sender.email}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${sender.email}</a>) has accepted your connection request on co/ment! <br />You have each other's email addresses now, so you're on your own from here. Good luck!`;
+                    break;
+                  case 'decline':
+                    boilerplate = `Connection Update: <strong>${sender.username}</strong> has declined your Connection request on co/ment`;
+                    break;
+                  case 'deactivate':
+                    boilerplate = `Connection Update: The mentorship between <strong>${sender.username}</strong> and <strong>${recipient.username}</strong> has ended`;
+                    break;
+                  default:
+                    boilerplate = `Great news! <strong>${sender.username}</strong> (<a class="link" href="mailto:${sender.email}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${sender.username}</a>) has responded to your ad on co/ment!`;
+                }
+                const params = {
+                    to      : recipientList,
+                    subject : req.body.subject,
+                    body    : {
+                        type : 'html',
+                        text : emailTpl.contactTemplate(
+                                recipient.username,          // toUser
+                                sender.username,             // fromUser
+                                sender.email,                // fromEmail
+                                sanitize(req.body.body),     // bodyText
+                                req.body.connectionId,       // connectionId
+                                boilerplate                 // custom text by email type
+                               )
+                    }
+                };
+
+                // send mail using `mailer` util
+                try {
+                    mailer(params.to, params.subject, params.body);
+                    return res
+                        .status(200)
+                        .json({ message : 'Message sent successfully.'});
+                } catch (err) {
+                    console.log(`Error: ${err}`);
                     return res
                         .status(400)
                         .json({ message : 'Error: Message not sent.'});
@@ -95,11 +190,11 @@ function sendEmail(req, res) {
 //   Returns: success message on success
 //
 function resendValidation(req, res) {
-    
+
     const target  = { _id : req.token._id };
     const updates = { signupKey : mailUtils.makeSignupKey() };
     const options = { new : true };
-    
+
     User.findOneAndUpdate(target, updates, options)
         .exec()
         .then( user => {
@@ -110,7 +205,7 @@ function resendValidation(req, res) {
                 type: 'html',
                 text: emailTpl.validationTemplate(url)
             };
-        
+
             // send mail using `mailer` util
             try {
                 mailer(user.email, subject, body);
@@ -128,10 +223,10 @@ function resendValidation(req, res) {
                 .status(400)
                 .json({ message: err});
         });
-    
+
 }
 
 
 /* ============================== EXPORT API =============================== */
 
-module.exports = { sendEmail, resendValidation };
+module.exports = { sendEmail, resendValidation, sendEmail2 };
--- utils/mailtemplates.js
+++ utils/mailtemplates.js
@@ -164,9 +164,9 @@ const longEmail = (title, url, toUser, subhead, bodyText, buttonText) => {
                     <tbody>
                       <tr>
                         <td style="color:#545454;" class="">
-                        <p style="color: #545454; font-size: 1.4em; line-height: 1.5em;font-weight:300; font-family: 'Alegreya Sans', sans-serif;text-align:center;">${{subhead}}</p>
+                        <p style="color: #545454; font-size: 1.4em; line-height: 1.5em;font-weight:300; font-family: 'Alegreya Sans', sans-serif;text-align:center;">${subhead}</p>
 
-                        <p style="color: 545454; font-size: 1.2em; line-height: 1.4em;font-weight:300; text-align: left; padding: 0 20px;">${bodyText}</p>
+                        <p style="color: 545454; font-size: 1.2em; line-height: 1.4em;font-weight:300; text-align: left; padding: 0 20px;">Personal message: <br />${bodyText}</p>
                         <br>
                         &nbsp;</td>
                       </tr>
@@ -445,12 +445,13 @@ const validationTemplate = (url) => {
 };
 
 // longEmail( title, url, toUser, subhead, bodyText, buttonText )
-const contactTemplate = (toUser, fromUser, fromEmail, bodyText, connectionId) => {
+const contactTemplate = (toUser, fromUser, fromEmail, bodyText, connectionId, boilerplate) => {
+  console.log(boilerplate)
   return longEmail(
     'co/ment Connection Request Email',
     `https://co-ment.glitch.me/connectiondetails/${connectionId}`,
     toUser,
-    `Great news! <strong>${fromUser}</strong> (<a class="link" href="mailto:${fromEmail}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${fromEmail}</a>) has responded to your ad on co/ment!`,
+    boilerplate || `Great news! <strong>${fromUser}</strong> (<a class="link" href="mailto:${fromEmail}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${fromEmail}</a>) has responded to your ad on co/ment!`,
     bodyText,
     'View Connection');
 };
