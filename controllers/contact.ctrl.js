0fee735ba5cf13b9ba6ee83c3b41e1e3addd6959
Jay Schwane <jrschwane@uwalumni.com>
2017-09-13 15:43:25 -0500

    Update sendEmail route handler
    Removes an unnecessary '.then()' block that was improperly returning an error response to the following '.then()' block. Catches thrown error if target recipient not found in database. Moves generation of connection status message boilerplate to /utils/mailutils.js. Refactors imported email utilities; now imports them via destructuring operator.

--- controllers/contact.ctrl.js
+++ controllers/contact.ctrl.js
@@ -7,98 +7,29 @@
 const User      = require('../models/user');
 const sanitize  = require('../utils/sanitizer');
 const mailer    = require('../utils/mailer');
-const mailUtils = require('../utils/mailutils');
 const emailTpl  = require('../utils/mailtemplates');
+const { makeSignupKey, makeValidationUrl, makeBoilerplate } = require('../utils/mailutils');
 
 
 /* ============================ ROUTE HANDLERS ============================= */
 
 // SEND EMAIL
-//   Example: POST >> /api/contact/597dd8665229970e99c6ab55
-//   Secured: yes, valid JWT required
-//   Expects:
-//     1) '_id' from JWT token
-//     2) 'id' from request params
-//   Returns: success message on success
-//
-function sendEmail(req, res) {
-    // prohibit users from contacing themselves
-    if (req.token._id === req.params.id) {
-        return res
-            .status(400)
-            .json({ message : 'You cannot contact yourself!'});
-    }
-
-    const target = { _id: req.params.id };
-    const fromId = { _id: req.token._id };
-
-    // find the target recipient
-    User.findOne(target)
-        .exec()
-        .then(recipient => {
-            if (!recipient) {
-                return res
-                    .status(404)
-                    .json({ message : 'User not found!'});
-            } else {
-                return recipient;
-            }
-        })
-        .then(recipient => {
-
-            // find the sender (we need their email address)
-            User.findOne(fromId, (err, sender) => {
-
-                if (err) { throw err; }
-
-                const params = {
-                    to      : recipient.email,
-                    subject : `co/ment - Contact Request from ${sender.username}`,
-                    body    : {
-                        type : 'html',
-                        text : emailTpl.contactTemplate(
-                                recipient.username,          // toUser
-                                sender.username,             // fromUser
-                                sender.email,                // fromEmail
-                                sanitize(req.body.bodyText), // bodyText
-                                req.body.connectionId        // connectionId
-                               )
-                    }
-                };
-
-
-                // send mail using `mailer` util
-                try {
-                    mailer(params.to, params.subject, params.body);
-                    return res
-                        .status(200)
-                        .json({ message : 'Message sent successfully.'});
-                } catch (err) {
-                    console.log(`Error: ${err}`);
-                    return res
-                        .status(400)
-                        .json({ message : 'Error: Message not sent.'});
-                }
-
-            });
-
-        });
-}
-
-// SEND EMAIL2
 //   Example: POST >> /api/sendemail
 //   Secured: yes, valid JWT required
 //   Expects:
-//     1) Object with email options
-//        { recipient: String (username),
-//          sender: String (username),
-//          copySender: boolean,
-//          subject: String,
-//          body: String,
-//          type: String,
-//    };
+//     1) sender's username' from JWT token
+//     2) request body properties : {
+//          recipient    : String (username)
+//          sender       : String (username)
+//          copySender   : Boolean
+//          subject      : String
+//          body         : String
+//          type         : String
+//          connectionId : String
+//        }
 //   Returns: success message on success
-function sendEmail2(req, res) {
+//
+function sendEmail(req, res) {
 
     // prohibit users from contacing themselves
     if (req.token.username === req.body.recipient) {
@@ -107,43 +38,25 @@ function sendEmail2(req, res) {
             .json({ message : 'You cannot contact yourself!'});
     }
 
-    const target = { username: req.body.recipient };
-    const fromId = { username: req.body.sender };
+    const toUser   = { username: req.body.recipient };
+    const fromUser = { username: req.body.sender };
 
     // find the target recipient
-    User.findOne(target)
+    User.findOne(toUser)
         .exec()
         .then(recipient => {
-           if (!recipient) {
-             throw new Error('User not found!');
-            } else {
-                return recipient;
+
+            if (!recipient) {
+                throw new Error('User not found!');
             }
-        })
-        .then(recipient => {
+
             // find the sender (we need their email address)
-            User.findOne(fromId, (err, sender) => {
+            User.findOne(fromUser, (err, sender) => {
 
                 if (err) { throw err; }
 
                 const recipientList = req.body.copySender ? `${recipient.email};${sender.email}` : recipient.email;
-                let boilerplate = '';
-                switch(req.body.type) {
-                  case 'request':
-                    boilerplate = `Great news! <strong>${sender.username}</strong> (<a class="link" href="mailto:${sender.email}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${sender.email}</a>) has responded to your ad on co/ment!`;
-                    break;
-                  case 'accept':
-                    boilerplate = `Great news! <strong>${sender.username}</strong> (<a class="link" href="mailto:${sender.email}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${sender.email}</a>) has accepted your connection request on co/ment! <br />You have each other's email addresses now, so you're on your own from here. Good luck!`;
-                    break;
-                  case 'decline':
-                    boilerplate = `Connection Update: <strong>${sender.username}</strong> has declined your Connection request on co/ment`;
-                    break;
-                  case 'deactivate':
-                    boilerplate = `Connection Update: The mentorship between <strong>${sender.username}</strong> and <strong>${recipient.username}</strong> has ended`;
-                    break;
-                  default:
-                    boilerplate = `Great news! <strong>${sender.username}</strong> (<a class="link" href="mailto:${sender.email}" style="color: #4981C2; text-decoration: none; border-bottom: 1px transparent;">${sender.username}</a>) has responded to your ad on co/ment!`;
-                }
+
                 const params = {
                     to      : recipientList,
                     subject : req.body.subject,
@@ -155,7 +68,7 @@ function sendEmail2(req, res) {
                                 sender.email,                // fromEmail
                                 sanitize(req.body.body),     // bodyText
                                 req.body.connectionId,       // connectionId
-                                boilerplate                 // custom text by email type
+                                makeBoilerplate(req.body.type, sender, recipient)
                                )
                     }
                 };
@@ -177,9 +90,9 @@ function sendEmail2(req, res) {
 
         })
         .catch((err) => {
-          return res
-            .status(404)
-            .json({ message : err.toString()});
+            return res
+                .status(404)
+                .json({ message : err.toString()});
         });
 }
 
@@ -194,14 +107,14 @@ function sendEmail2(req, res) {
 function resendValidation(req, res) {
 
     const target  = { _id : req.token._id };
-    const updates = { signupKey : mailUtils.makeSignupKey() };
+    const updates = { signupKey : makeSignupKey() };
     const options = { new : true };
 
     User.findOneAndUpdate(target, updates, options)
         .exec()
         .then( user => {
 
-            const url = mailUtils.makeValidationUrl(user._id, user.signupKey.key);
+            const url = makeValidationUrl(user._id, user.signupKey.key);
             const subject = 'co/ment - Email verification required';
             const body    = {
                 type: 'html',
@@ -231,4 +144,4 @@ function resendValidation(req, res) {
 
 /* ============================== EXPORT API =============================== */
 
-module.exports = { sendEmail, resendValidation, sendEmail2 };
+module.exports = { sendEmail, resendValidation };
