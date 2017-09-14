17c32a56f0f6f32a167de83b4d30bcd40683e5d9
Sarah Schneider <rifkegribenes@gmail.com>
2017-09-13 22:54:43 -0700

    add footer

---
+++ client/src/containers/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => (
+      <div className="footer">
+        &copy; 2017 The Ursa Majors
+        <a
+        	href="https://github.com/ursa-majors/co-ment"
+        	rel="noopener noreferrer"
+          target="_blank"
+          className="footer__link">
+          <i className="fa fa-github footer__icon"></i>
+        </a>
+      </div>
+    );
+
+export default Footer;
