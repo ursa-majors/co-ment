6e4ba5657fb7bad1bb8c323f4fb5c4e20152e111
genestd <drwgenest@gmail.com>
2017-09-18 21:40:05 -0700

    Cleaning linting errors

--- client/src/containers/Footer.jsx
+++ client/src/containers/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const Footer = () => (
-      <div className="footer">
-        &copy; 2017 The Ursa Majors
-        <a
-        	href="https://github.com/ursa-majors/co-ment"
-          data-taborder=""
-        	rel="noopener noreferrer"
-          target="_blank"
-          className="footer__link">
-          <i className="fa fa-github footer__icon"></i>
-        </a>
-      </div>
-    );
+  <div className="footer">
+    &copy; 2017 The Ursa Majors
+    <a
+      href="https://github.com/ursa-majors/co-ment"
+      data-taborder=""
+      rel="noopener noreferrer"
+      target="_blank"
+      className="footer__link"
+    >
+      <i className="fa fa-github footer__icon" />
+    </a>
+  </div>
+);
 
 export default Footer;
