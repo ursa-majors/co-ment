168e04fba6314e5584d3376831c0110f676032ef
genestd <drwgenest@gmail.com>
2017-09-18 21:44:19 -0700

    Removing more lint

--- client/src/containers/NotFound.jsx
+++ client/src/containers/NotFound.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-      <div className="container">
-        <div className="notfound">
-          <div className="notfound__header">page not found</div>
-          <div className="notfound__text">Sorry, there's nothing here!</div>
-          <div className="notfound__text">Want to go <Link to='/' className="notfound__link">home</Link>?</div>
-        </div>
+  <div className="container">
+    <div className="notfound">
+      <div className="notfound__header">page not found</div>
+      <div className="notfound__text">{'Sorry, there\'s nothing here!'}</div>
+      <div className="notfound__text">
+        Want to go <Link to="/" className="notfound__link">home</Link>?
       </div>
-    );
+    </div>
+  </div>
+);
 
 export default NotFound;
