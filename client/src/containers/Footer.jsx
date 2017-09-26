3e146180d4ff8ef1ec176aea1f4a5f17a4e773e5
genestd <drwgenest@gmail.com>
2017-09-26 14:24:58 -0700

    Add mobile prop to footer - make privacy policy link responsive.

--- client/src/containers/Footer.jsx
+++ client/src/containers/Footer.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Footer = () => (
+const Footer = props => (
   <div className="footer">
     &copy; 2017 The Ursa Majors
-    <div className="">
-      <Link className="footer__link" to="/privacy">Privacy</Link>
+    <div>
+      <Link className="footer__link" to="/privacy">
+        {props.mobile ? <i className="fa fa-user-secret footer__icon" /> : 'Privacy'}
+      </Link>
+      <a
+        href="https://github.com/ursa-majors/co-ment"
+        data-taborder=""
+        rel="noopener noreferrer"
+        target="_blank"
+        className="footer__link"
+      >
+        <i className="fa fa-github footer__icon" />
+      </a>
     </div>
-    <a
-      href="https://github.com/ursa-majors/co-ment"
-      data-taborder=""
-      rel="noopener noreferrer"
-      target="_blank"
-      className="footer__link"
-    >
-      <i className="fa fa-github footer__icon" />
-    </a>
   </div>
 );
 
