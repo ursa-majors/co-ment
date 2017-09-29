5c1803d3e18a7f18d6eddacd0623143ccf1daca9
genestd <drwgenest@gmail.com>
2017-09-28 17:18:37 -0700

    Change api endpoint to use env variable so we can build for diff envs

--- client/src/store/actions/apiActions.js
+++ client/src/store/actions/apiActions.js
@@ -1,4 +1,7 @@
 import { CALL_API } from 'redux-api-middleware';
+// ENVIRONMENT is a global variable defined by weback.config.js
+// defaults to DEVELOPMENT
+const BASE_URL = (ENVIRONMENT === 'PRODUCTION' ? 'https://co-ment.glitch.me' : 'https://co-ment-dev.glitch.me');
 
 export const SEND_EMAIL_REQUEST = 'SEND_EMAIL_REQUEST';
 export const SEND_EMAIL_SUCCESS = 'SEND_EMAIL_SUCCESS';
@@ -7,7 +10,7 @@ export const SEND_EMAIL_FAILURE = 'SEND_EMAIL_FAILURE';
 export function sendEmail(token, mailOptions) {
   return {
     [CALL_API]: {
-      endpoint: 'https://co-ment.glitch.me/api/sendemail/',
+      endpoint: `${BASE_URL}/api/sendemail/`,
       method: 'POST',
       types: [SEND_EMAIL_REQUEST, SEND_EMAIL_SUCCESS, SEND_EMAIL_FAILURE],
       headers: { Authorization: `Bearer ${token}`,
@@ -25,7 +28,7 @@ export const GET_PROFILE_FAILURE = 'GET_PROFILE_FAILURE';
 export function getProfile(token, profileId) {
   return {
     [CALL_API]: {
-      endpoint: `https://co-ment.glitch.me/api/profile/${profileId}`,
+      endpoint: `${BASE_URL}/api/profile/${profileId}`,
       method: 'GET',
       types: [GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, GET_PROFILE_FAILURE],
       headers: { Authorization: `Bearer ${token}` },
@@ -40,7 +43,7 @@ export const MODIFY_PROFILE_FAILURE = 'MODIFY_PROFILE_FAILURE';
 export function modifyProfile(token, profileId, body) {
   return {
     [CALL_API]: {
-      endpoint: `https://co-ment.glitch.me/api/profile/${profileId}`,
+      endpoint: `${BASE_URL}/api/profile/${profileId}`,
       method: 'PUT',
       types: [MODIFY_PROFILE_REQUEST, MODIFY_PROFILE_SUCCESS, MODIFY_PROFILE_FAILURE],
       headers: { Authorization: `Bearer ${token}`,
@@ -75,7 +78,7 @@ export const RESEND_ACCT_VALIDATION_FAILURE = 'RESEND_ACCT_VALIDATION_FAILURE';
 export function resendAcctValidation(token) {
   return {
     [CALL_API]: {
-      endpoint: 'https://co-ment.glitch.me/api/resendvalidation',
+      endpoint: `${BASE_URL}/api/resendvalidation`,
       method: 'GET',
       headers: { Authorization: `Bearer ${token}` },
       types: [
