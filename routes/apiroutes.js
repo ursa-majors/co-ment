3e569c8fd72b590332d62efe2c6f2ba8662124e0
Jay Schwane <jrschwane@uwalumni.com>
2017-11-12 11:41:53 -0600

    Removes '/api' from route definition paths

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -10,7 +10,16 @@ const jwt    = require('express-jwt');
 const auth   = jwt({ secret: secret, requestProperty: 'token' });
 
 
-/* ========================= INIT ROUTE MIDDLEWARE ========================= */
+/* =========================== LOAD CONTROLLERS ============================ */
+
+const profileCtrl      = require('../controllers/profile.ctrl');
+const postCtrl         = require('../controllers/post.ctrl');
+const contactCtrl      = require('../controllers/contact.ctrl');
+const connectionCtrl   = require('../controllers/connection.ctrl');
+const conversationCtrl = require('../controllers/conversation.ctrl');
+
+
+/* =========================== ROUTE MIDDLEWARE ============================ */
 
 // Checks wheather user has validated their account.
 // If `validated: false`, bail out early.
@@ -29,111 +38,108 @@ function checkValidated(req, res, next) {
 }
 
 
-/* =========================== INIT CONTROLLERS ============================ */
-
-const profileCtrl      = require('../controllers/profile.ctrl');
-const postCtrl         = require('../controllers/post.ctrl');
-const contactCtrl      = require('../controllers/contact.ctrl');
-const connectionCtrl   = require('../controllers/connection.ctrl');
-const conversationCtrl = require('../controllers/conversation.ctrl');
-
+// Checks existence and validity of JWT token
+router.use(auth);
 
 /* ================================ ROUTES ================================= */
 
 
 // Get a user's profile
 // Returns JSON user profile object on success
-router.get('/api/profile/:id', [auth], profileCtrl.getOneProfile);
+router.get('/profile/:id', profileCtrl.getOneProfile);
 
 
 // Update a user's profile.
 // Returns updated JSON user profile object on success.
-router.put('/api/profile/:id', [auth], profileCtrl.updateProfile);
+router.put('/profile/:id', profileCtrl.updateProfile);
 
 
 // Delete a user and all their posts
 // Returns deleted user profile
-router.delete('/api/profile/:id', [auth], profileCtrl.deleteProfile);
+router.delete('/profile/:id', profileCtrl.deleteProfile);
 
 
 // Get posts
 // Returns JSON array of 'post' objects on success.
-router.get('/api/posts', [auth], postCtrl.getPosts);
+router.get('/posts', postCtrl.getPosts);
 
 
 // Increment Post views
 // Returns success status code; returns no data
-router.put('/api/posts/:id/viewsplusplus', [auth], postCtrl.incPostViews);
+router.put('/posts/:id/viewsplusplus', postCtrl.incPostViews);
 
 
 // Resend user validation email
 // Returns success message
-router.get('/api/resendvalidation', [auth], contactCtrl.resendValidation);
+router.get('/resendvalidation', contactCtrl.resendValidation);
 
+
+// ===== MIDDLEWARE FOR SUBSEQUENT ROUTES ONLY =====
+router.use(checkValidated);
 
 // Get all user profiles
 // Returns an array of user profile objects
-router.get('/api/profiles', [auth, checkValidated], profileCtrl.getProfiles);
+router.get('/profiles', profileCtrl.getProfiles);
 
 
 // Create a post
 // Returns new post object
-router.post('/api/posts', [auth, checkValidated], postCtrl.createPost);
+router.post('/posts', postCtrl.createPost);
 
 
 // Update a post
 // Returns updated post object
-router.put('/api/posts/:id', [auth, checkValidated], postCtrl.updatePost);
+router.put('/posts/:id', postCtrl.updatePost);
 
 
 // Increment or decrement a post's likes
 // Returns sucess status code only
-router.put('/api/posts/:id/likes', [auth, checkValidated], postCtrl.updatePostLikes);
+router.put('/posts/:id/likes', postCtrl.updatePostLikes);
 
 
 // Delete a post
 // Returns deleted post object
-router.delete('/api/posts/:id', [auth, checkValidated], postCtrl.deletePost);
+router.delete('/posts/:id', postCtrl.deletePost);
 
 
 // Send contact email to another user
 // Returns success message
-router.post('/api/sendemail', [auth, checkValidated], contactCtrl.sendEmail);
+router.post('/sendemail', contactCtrl.sendEmail);
 
 
 // Get all connections where the user is either a mentor or mentee
 // Returns success message
-router.get('/api/connections', [auth, checkValidated], connectionCtrl.getConnections);
+router.get('/connections', connectionCtrl.getConnections);
 
 
 // Create a new connection record
 // Returns success message on success.
-router.post('/api/connections', [auth, checkValidated], connectionCtrl.createConnection);
+router.post('/connections', connectionCtrl.createConnection);
 
 
 // Update a connection record's status & status date
 // Returns success message on success.
-router.put('/api/connections', [auth, checkValidated], connectionCtrl.updateConnection);
+router.put('/connections', connectionCtrl.updateConnection);
 
 
 // Get a list of a user's conversations
 // Returns array of conversations on success
-router.get('/api/conversations', [auth, checkValidated], conversationCtrl.getConversations);
+router.get('/conversations', conversationCtrl.getConversations);
 
 
 // Get all messages in one of the user's conversation
 // Returns array of messages on success
-router.get('/api/conversations/:id', [auth, checkValidated], conversationCtrl.getConversation);
+router.get('/conversations/:id', conversationCtrl.getConversation);
 
 
 // Create a new conversation
 // Returns success message & conversation '_id' on success.
-router.post('/api/conversations', [auth, checkValidated], conversationCtrl.createConversation);
+router.post('/conversations', conversationCtrl.createConversation);
 
 
 // Create a new message
 // Returns success message on success.
-router.post('/api/messages', [auth, checkValidated], conversationCtrl.postMessage);
+router.post('/messages', conversationCtrl.postMessage);
 
 
 /* ================================ EXPORT ================================= */
