b1a7af498fef225092a1cd8dc6212de537fda51e
Jay Schwane <jrschwane@uwalumni.com>
2017-11-25 09:45:42 -0600

    Keep 'engagement()' call commented out

--- server.js
+++ server.js
@@ -98,6 +98,6 @@ mongoose.Promise = global.Promise;
 /* ================================ STARTUP ================================ */
 
 app.listen(port, () => {
-    engagement();
+//    engagement();
     console.log(`Server listening on port ${port}.`);
 });
