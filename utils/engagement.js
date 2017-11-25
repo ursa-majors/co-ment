5532dc88b04a33061c62b5f3ee28a4613a93a140
Jay Schwane <jrschwane@uwalumni.com>
2017-11-25 09:48:49 -0600

    Tweaks & fixes to the user engagement utility
    Adds 'finalEligibility' filter to exclude users who have previously been contacted; Adds 'updateUserRecords' function to set appropriate user 'engagementMeta' field depending on what type of user engagement email needs to be sent; Updates almost all function documentation.

--- utils/engagement.js
+++ utils/engagement.js
@@ -13,12 +13,12 @@ const { engagementTpl } = require('./mailtemplates');
 
 /* =============================== UTILITIES =============================== */
 
-/* determine basic eligibility of each user
-   Checks:
-     user.createdAt (Date) - if > 1 week ago, user is eligible
-     contactMeta.unSubbed (Boolean) - if 'false', user is eligible
- * @params    [object]     user  [the user object from db ]
- * @returns   [boolean]          [true if user is eligible for email]
+/** determine basic eligibility of each user
+ *  Checks:
+ *    user.createdAt (Date) - if > 1 week ago, user is eligible
+ *    contactMeta.unSubbed (Boolean) - if 'false', user is eligible
+ *  @param     {object}     user   The user object from db
+ *  @returns   {boolean}           True if user is eligible for email
 */
 function basicEligibility(user) {
 
@@ -32,13 +32,13 @@ function basicEligibility(user) {
 }
 
 
-/* determine what type of engagement email to send eligibile user
-   Checks for existence of `user.name`, a required field when updating
-   a user profile.
- * @params    [object]    user   [the user object from db ]
- * @returns   [object]           [user object w/property 'engageType']
+/** determine what type of engagement email to send an eligibile user
+ *  Checks for existence of `user.name`, a required field when updating
+ *  a user profile.
+ *  @param     {object}    user   The user object from db
+ *  @returns   {object}           User object w/property 'engageType'
 */
-function addEligibilityType(user) {
+function addEngagementType(user) {
 
     if (!user.name) {
         user.engageType = 'profile';
@@ -50,9 +50,36 @@ function addEligibilityType(user) {
 }
 
 
-/* map/reduce posts to array of unique authors 
- * @params    [array]   posts  [array of post objects from db ]
- * @returns   [array]          [array of unique post author '_id's]
+/** determine final eligibility of each user
+ *  Purpose: don't contact users who have already been contacted re: `engageType`
+ *  Checks:
+ *    user.engageType (either 'profile' or 'post')
+ *    user.engagementMeta dates ('addProfileReminder' and 'addPostReminder')
+ *  @param     {object}     user   The user object from db
+ *  @returns   {boolean}           True if user is eligible for email
+*/
+function finalEligibility(user) {
+    
+    let prevProfileEngSent = Boolean(user.engagementMeta.addProfileReminder);
+    let prevPostEngSent    = Boolean(user.engagementMeta.addPostReminder);
+    
+    // fail if user has already been emailed to update their profile
+    if (user.engageType === 'profile' && prevProfileEngSent) {
+        return false;
+    }
+    
+    // fail if user has already been emailed to add a post
+    if (user.engageType === 'post' && prevPostEngSent) {
+        return false;
+    }
+    
+    return true;
+}
+
+
+/** map/reduce posts to array of unique authors 
+ *  @param     {array}   posts   Array of post objects from db
+ *  @returns   {array}           Array of unique post author '_id's
 */
 function makePostAuthorsArr(posts) {
 
@@ -75,7 +102,7 @@ function makePostAuthorsArr(posts) {
 /* ============================ PRIVATE METHODS ============================ */
 
 /** Check db logs for previous engagement run today
- *  @returns   [Promise]      [Promise object with Boolean payload]
+ *  @returns   {Promise}      Promise object with Boolean payload
 */
 function checkAlreadyRun() {
 
@@ -93,8 +120,8 @@ function checkAlreadyRun() {
 
             console.log('Last log taken: ', log[0].createdAt);
 
-//            return (today > lastLogDate + 86400) ? true : false;
-            return true;
+            return (today > lastLogDate + 86400) ? true : false;
+//            return true;
 
         });
 
@@ -102,18 +129,18 @@ function checkAlreadyRun() {
 
 
 /** Get inactive users
- * Finds all users, then filters for users who have NOT authored posts.
- * @param     [array]     postAuthors   [all unique post authors]
- * @returns   [Promise]                 [Promise w/array of inactive users]
+ *  Finds all users, then filters for users who have NOT authored posts.
+ *  @param     {array}     postAuthors   All unique post authors
+ *  @returns   {Promise}                 Promise w/array of inactive users
 */
 function getInactiveUsers(postAuthors) {
 
     const projection = {
-        _id : 1,
-        username : 1,
-        name : 1,
-        email : 1,
-        contactMeta : 1,
+        _id            : 1,
+        username       : 1,
+        name           : 1,
+        email          : 1,
+        contactMeta    : 1,
         engagementMeta : 1
     };
 
@@ -128,14 +155,17 @@ function getInactiveUsers(postAuthors) {
             return postAuthors.indexOf(u._id.toString()) === -1;
         }))
 
-        // from this ^ subset, determine what type of engagement email to send
-        .then( users => users.map(addEligibilityType));
+        // determine what type of engagement email to send
+        .then( users => users.map(addEngagementType))
+
+        // filter based on whether previous engagement email was sent
+        .then( users => users.filter(finalEligibility) );
 
 }
 
 
 /** Get list of all post authors
- * @returns   [Promise]   [Promise object w/filtered list of post authors]
+ *  @returns   {Object}   Promise object w/filtered list of post authors
 */
 function getPostAuthors() {
 
@@ -147,8 +177,8 @@ function getPostAuthors() {
 
 
 /** Send email to applicable users
- * @param     [array]     users   [all inactive users]
- * @returns   [array]             [all emailed users]
+ *  @param     {array}     users   All inactive users
+ *  @returns   {array}             All emailed users
 */
 function sendEmail(users) {
 
@@ -182,9 +212,44 @@ function sendEmail(users) {
 }
 
 
+/** update user records 
+ *  Set appropriate `engagementMeta` property depending on `engageType`.
+ *  @param     {array}   users     All inactive users
+ *  @returns   {array}             All inactive users
+*/
+function updateUserRecords(users) {
+    
+    const pool = users.map( user => {
+
+        let updates = {};
+
+        if (user.engageType === 'profile') {
+            updates['engagementMeta.addProfileReminder'] = new Date().toISOString();
+        }
+
+        if (user.engageType === 'post') {
+            updates['engagementMeta.addPostReminder'] = new Date().toISOString();
+        }
+
+        return User.findByIdAndUpdate(user._id, updates)
+            .exec();
+        
+    });
+
+    return Promise.all(pool)
+        .then( () => {
+            if (pool.length > 0) {
+                console.log(`Updated ${pool.length} user records.`);
+            }
+            return users;
+        });
+
+}
+
+
 /** save operation log to database
- * @param     [array]   users     [all inactive users]
- * @returns   [array]             [all inactive users]
+ *  @param     {array}   users     All inactive users
+ *  @returns   {array}             All inactive users
 */
 function saveLog(users) {
 
@@ -194,25 +259,25 @@ function saveLog(users) {
 		actionTaken: 'dispatched_emails'
     });
 
-//    newLog.save( (err, result) => {
-//        if (err) { throw new Error(err); }
-//    });
+    newLog.save( (err, result) => {
+        if (err) { throw new Error(err); }
+    });
 
     return users;
 }
 
 
 /** log some stats
- * @param   [array]   users   [all inactive users]
+ *  @param     {array}   users     All inactive users
 */
 function logStats(users) {
     console.log(`
 ***************************************************
                       STATS
 ***************************************************
-${users.length} users meet 'inactive' criteria:
+${users.length} users meet 'inactive' criteria...
 
-${users.map( u => u._id + ' : ' + u.username + ' ( ' + u.email + ' )').join('\n')}
+${users.map( u => u._id + ' : ' + u.username + ' ( ' + u.email + ' ) ' + u.engageType).join('\n')}
     `);
 }
 
@@ -223,13 +288,14 @@ module.exports = function() {
     
     // first, check logs. If last log.createAd = today, abort
     checkAlreadyRun()
-        .then(result => {
+        .then(okToProceed => {
             
-            if (result === true) {
+            if (okToProceed) {
                 getPostAuthors()
                     .then(getInactiveUsers)
                     .then(sendEmail)
                     .then(saveLog)
+                    .then(updateUserRecords)
                     .then(logStats);
             } else {
                 return Promise.reject('Too soon to send engagement emails');
