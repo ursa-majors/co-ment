a750b691e75f71c70e44156591e7201c6cd63dac
Jay Schwane <jrschwane@uwalumni.com>
2017-11-29 21:39:45 -0600

    Adds middleware to redirect insecure http connections to secure https

---
+++ config/force-https.js
@@ -0,0 +1,14 @@
+/*
+    Middleware to force / redirect to https connections
+*/
+
+module.exports = function (req, res, next) {
+    
+    if (req.headers['x-forwarded-proto'].match(/https/g)) {
+        // Secure protocol, continue as normal
+        return next();
+    }
+    
+    res.redirect(302, `https://${req.hostname}${req.url}`);
+    
+};
