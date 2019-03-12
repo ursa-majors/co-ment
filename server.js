c30f1c56124f62ffab685c8b4005423b64f910ff
Jay Schwane <jrschwane@uwalumni.com>
2019-03-11 23:59:48 -0500

    Clean up server

--- server.js
+++ server.js
@@ -1,111 +1,56 @@
-/* Co/Ment API
-     (c) 2017 - The Ursa Majors
-     https://github.com/ursa-majors/co-ment
-*/
+'use strict'
 
-/* ================================= SETUP ================================= */
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const compression = require('compression')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose')
 
-const express       = require('express');
-const app           = express();
-const morgan        = require('morgan');
-const bodyParser    = require('body-parser');
-const path          = require('path');
-const comentCors    = require('./config/cors');
-const compression   = require('compression');
-const forceHttps    = require('./config/force-https');
+const corsConfig = require('./config/cors')
+const forceHttps = require('./config/force-https')
+const strategy = require('./config/strategy')
+const db = require('./db')
+const User = require('./models/user')
 
-// passport auth
-const passport      = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const strategy      = require('./config/strategy');
+const { requestMiddleware, loggerMiddleware, errorMiddleware } = require('./middleware')
 
-// db
-const db            = require('./db');
-const mongoose      = require('mongoose');
-
-// db models
-const User          = require('./models/user');
-const Post          = require('./models/post');
-
-// routes
-const apiRoutes     = require('./routes/apiroutes');
-const authRoutes    = require('./routes/authroutes');
-const staticRoutes  = require('./routes/staticroutes');
-
-// error handler
-const errorHandler  = require('./utils/errorhandler');
+const app = express()
+const PORT = process.env.PORT || 3001
 
 // email functions
-const engagement    = require('./utils/engagement');
+// const engagement = require('./utils/engagement')
 
-// port
-const port          = process.env.PORT || 3001;
+// middleware
+if (process.env.NODE_ENV === 'production') {
+  app.use(forceHttps)
+}
+app.use(loggerMiddleware())
+app.use(requestMiddleware())
+app.use(compression())
+app.use(corsConfig)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/client/build/')))
+app.use(passport.initialize())
+passport.use(strategy(LocalStrategy, User))
 
+// routes
+app.use('/auth', require('./routes/authroutes'))
+app.use('/api', require('./routes/apiroutes'))
+app.use('/', require('./routes/staticroutes'))
 
-/* ============================= CONFIGURATION ============================= */
+// catch-all error handler
+app.use(errorMiddleware())
 
-// force https
-app.use(forceHttps);
-
-// gzip responses
-app.use(compression());
-
-// enable logger
-app.use(morgan('dev'));
-
-// enable http request body parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }));
-
-// set static path
-app.use(express.static(path.join(__dirname, '/client/build/')));
-
-
-/* ================================= CORS ================================= */
-
-app.use(comentCors);
-
-
-/* =============================== PASSPORT ================================ */
-
-app.use(passport.initialize());
-passport.use(strategy(LocalStrategy, User));
-
-
-/* ================================ ROUTES ================================= */
-
-app.use('/api', authRoutes);
-app.use('/api', apiRoutes);
-app.use(staticRoutes);
-
-
-/* ============================= ERROR HANDLER ============================= */
-
-app.use(errorHandler);
-
-
-/* ============================= CONNECT TO DB ============================= */
-
-// new Mongo ( >= 4.11.0 ) connection logic:
-mongoose.connect(db.getDbConnectionString(), {
-    useMongoClient: true
-});
-
-// old Mongo connection logic (may be needed for Heroku):
-//mongoose.connect(db.getDbConnectionString());
-
-// log mongoose connection errors to console
-mongoose.connection.on('error', function(err) {
-    console.error('Mongoose connection error: ', err);
-});
-
-// tell Mongoose to use Node global es6 Promises
-mongoose.Promise = global.Promise;
-
-
-/* ================================ STARTUP ================================ */
-
-app.listen(port, () => {
-    engagement();
-    console.log(`Server listening on port ${port}.`);
-});
+// connect to db and startup
+mongoose.connect(db.getDbConnectionString(), { useNewUrlParser: true })
+  .then(() => {
+    console.log(`Connected to ${db.getDbConnectionString()}`)
+    app.listen(PORT, () => console.log('Server listening on port:', PORT))
+  })
+  .catch((err) => {
+    console.log(err.message)
+    process.exit(1)
+  })
