61f7c45ec2b7179b08ef26ef3e6f9510a05527c6
Jay Schwane <jrschwane@uwalumni.com>
2019-03-14 00:26:07 -0500

    Tweaks request log formatting

--- middleware/request.js
+++ middleware/request.js
@@ -3,13 +3,13 @@
 const { STATUS_CODES } = require('http')
 
 exports = module.exports = () => (req, res, next) => {
-  req.log.info(`Received ${req.method.toUpperCase()} request for: ${req.url}`)
+  req.log.info(`Received ${req.method.toUpperCase()} >> ${req.url}`)
 
   // on 'finish' events, log the response
   res.on('finish', () => {
     const statusMessage = STATUS_CODES[res.statusCode]
     const level = res.statusCode < 400 ? 'info' : 'error'
-    req.log[level](`Resolved ${req.method.toUpperCase()}; ${res.statusCode} ${statusMessage}`)
+    req.log[level](`Resolved ${req.method.toUpperCase()} >> ${res.statusCode} ${statusMessage}`)
   })
   next()
 }
