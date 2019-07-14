e4e646f19a66adae13b1b7b4e43522d7f98a8465
Jay Schwane <jrschwane@uwalumni.com>
2019-07-14 18:46:40 -0500

    Simplify hot module reload logic

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,25 +1,17 @@
+import 'react-hot-loader'
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
-import Redbox from 'redbox-react';
 
-// import reducer from './reducers';
 import store from './store/store';
 import App from './App';
 import './style.scss';
-
-delete AppContainer.prototype.unstable_handleError;
 
 const root = document.getElementById('root');
 
 render(
   <Provider store={store}>
-    <AppContainer>
-      <App />
-    </AppContainer>
+    <App />
   </Provider>,
   root,
 );
-
-if (module.hot) module.hot.accept(App, () => render(App));
